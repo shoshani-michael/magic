3200b88c016e07e3773e1760167f471a23501728
Michael Shoshani <shoshani.michael@gmail.com>
2025-08-24 16:36:49 +0300

    changed to gemma270
    changed to a smaller model for faster testing

--- experiments/configs/template.py
+++ experiments/configs/template.py
@@ -22,10 +22,10 @@ def get_config():
     config.result_prefix = 'results/individual_vicuna7b'
 
     # tokenizers
-    config.tokenizer_paths=['lmsys/vicuna-7b-v1.5']
+    config.tokenizer_paths=['google/gemma-3-270m']
     config.tokenizer_kwargs=[{"use_fast": False}]
     
-    config.model_paths=['lmsys/vicuna-7b-v1.5']
+    config.model_paths=['google/gemma-3-270m']
     config.model_kwargs=[{"low_cpu_mem_usage": True, "use_cache": False}]
     config.conversation_templates=['vicuna']
     config.devices=['cuda:0']
